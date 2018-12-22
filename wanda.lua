0533783bbfa8412634a4d282590aab91f76c6e46
Chris Pressey <cpressey@catseye.tc>
2018-12-22 22:37:33 +0000

    Refactor to use functions

--- wanda.lua
+++ wanda.lua
@@ -1,15 +1,17 @@
-local filename = arg[1]
-local file = io.open(filename)
-local program = file:read("*all")
-io.close(file)
-
-redex = {}
-
-for token in string.gmatch(program, "[^%s]+") do
-   table.insert(redex, token)
+function parse_program(program)
+    redex = {}
+    for token in string.gmatch(program, "[^%s]+") do
+       table.insert(redex, token)
+    end
+    return redex
 end
 
-rules = {}  -- not really
+function load_program(filename)
+    local file = io.open(filename)
+    local program = file:read("*all")
+    io.close(file)
+    return parse_program(program)
+end
 
 function find_match(rules, redex, i)
     local j = i
@@ -23,22 +25,34 @@ function find_match(rules, redex, i)
     end
 end
 
-start_index = 1
-while start_index < table.getn(redex) do
-    match_info = find_match(rules, redex, start_index)
-    if match_info ~= nil then
-        local i = match_info[1]
-        local j = match_info[2]
-        while i <= j do
-            table.remove(redex, i)
-            j = j - 1
+function run_wanda(redex)
+    rules = {}
+    start_index = 1
+    while start_index < table.getn(redex) do
+        match_info = find_match(rules, redex, start_index)
+        if match_info ~= nil then
+            local i = match_info[1]
+            local j = match_info[2]
+            while i <= j do
+                table.remove(redex, i)
+                j = j - 1
+            end
+            -- TODO: insert
+            -- TODO: also apply the side-effect
+            start_index = 1
+        else
+            start_index = start_index + 1
         end
-        -- TODO: insert
-        -- TODO: also apply the side-effect
-        start_index = 1
-    else
-        start_index = start_index + 1
     end
+    return redex
 end
 
-print(table.concat(redex, " "))
+function format_redex(redex)
+    return table.concat(redex, " ")
+end
+
+--[[========================= main ================= ]]--
+
+local program = load_program(arg[1])
+local result = run_wanda(program)
+print(format_redex(result))
