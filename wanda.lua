3361aed664d1e6f1a742fabb72471e0e716f77d8
Chris Pressey <cpressey@catseye.tc>
2018-12-23 07:22:14 +0000

    Actually make replacements now.

--- wanda.lua
+++ wanda.lua
@@ -13,16 +13,48 @@ function load_program(filename)
     return parse_program(program)
 end
 
+function is_number(atom)
+    return (atom ~= nil and string.find(atom, "^[+-]?%d+$"))
+end
+
+function format_redex(redex)
+    return table.concat(redex, " ")
+end
+
 function find_match(rules, redex, i)
     local j = i
+
     if redex[i] == "[" then
         while redex[j] ~= "]" and redex[j] ~= nil do
            j = j + 1
         end
-        return {i, j}
-    else
-        return nil  -- find first rule in rules that matches redex[i ... end]
+        return {i, j, {}}
     end
+
+    if is_number(redex[i]) and is_number(redex[i+1]) and redex[i+2] == "+" then
+        local a = tonumber(redex[i])
+        local b = tonumber(redex[i+1])
+        local r = tostring(a + b)
+        return {i, i+2, {r}}
+    end
+
+    if is_number(redex[i]) and is_number(redex[i+1]) and redex[i+2] == "*" then
+        local a = tonumber(redex[i])
+        local b = tonumber(redex[i+1])
+        local r = tostring(a * b)
+        return {i, i+2, {r}}
+    end
+
+    if is_number(redex[i]) and is_number(redex[i+1]) and redex[i+2] == "-" then
+        local a = tonumber(redex[i])
+        local b = tonumber(redex[i+1])
+        local r = tostring(a - b)
+        return {i, i+2, {r}}
+    end
+
+    -- else find first rule in rules that matches redex[i ... end]
+
+    return nil
 end
 
 function run_wanda(redex)
@@ -33,22 +65,25 @@ function run_wanda(redex)
         if match_info ~= nil then
             local i = match_info[1]
             local j = match_info[2]
+
             while i <= j do
                 table.remove(redex, i)
                 j = j - 1
             end
-            -- TODO: insert
+
+            local replacement = match_info[3]
+            for n, v in ipairs(replacement) do
+                table.insert(redex, i + (n-1), v)
+            end
+
             -- TODO: also apply the side-effect
+
             start_index = 1
         else
             start_index = start_index + 1
         end
     end
     return redex
-end
-
-function format_redex(redex)
-    return table.concat(redex, " ")
 end
 
 --[[========================= main ================= ]]--
